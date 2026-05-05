5733889f3fff0eda4d121ba770c7a424184318e6
alemorillo7 <alemorillocba7@gmail.com>
2026-05-05 19:11:56 -0300

    debug: sanitize and log service account string

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
     const privateKey = process.env.FIREBASE_PRIVATE_KEY;
     const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
     const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
-    
+
     if (privateKey && clientEmail && projectId) {
       try {
         admin.initializeApp({
@@ -32,7 +32,7 @@ if (!admin.apps.length) {
     console.error('Main Firebase admin initialization error', error);
   }
 }
-}
+
 
 
 export const adminDb = admin.firestore();
